d9cae0a4cb03ea3b648c2175cf3705b6049493bd
Alex Greig <78413528+alexjgreig@users.noreply.github.com>
2021-04-27 22:33:47 +1000

    Added functionality surrounding user config files

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,2 @@
+mod user;
 fn main() {}
--- src/user.rs
+++ src/user.rs
@@ -1,19 +1,68 @@
+use curve25519_dalek;
+use rand_core;
+use serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
+use std::io;
+use std::io::{Read, Write};
+use zeroize;
+
+#[derive(Serialize, Deserialize)]
+pub struct Keys {
+    public: Option<String>,
+    private: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct User {
     pub username: String,
-    pub EccKey: String,
-    pub IPv4: String,
+    pub keys: Keys,
+    pub ipv4: String,
 }
-impl User {
 
+impl User {
     pub fn new(username: String) {
         User {
-            username: username,
-            EccKey: create_ecc_key()
-            IPv4: create_ipv4()
+            username: get_username(),
+            keys: create_ecc_key(),
+            ipv4: create_ipv4(),
+        };
     }
-    pub fn store_credentials(&self) {}
+    pub fn create_user_config(&self) {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("config.toml")
+            .expect("Couldn't create TOML file.");
+        file.write(toml::to_string(&self).unwrap().as_bytes());
+    }
+
+    pub fn load_user_config(&mut self) {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .create(true)
+            .open("config.toml")
+            .expect("Couldn't open config file.");
+
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+        *self = toml::from_str(&contents).unwrap();
+    }
 }
 
-fn create_ecc_key() {}
+fn get_username() -> String {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    return input.trim().to_string();
+}
 
-fn create_ipv4() {}
+fn create_ecc_key() -> Keys {
+    Keys {
+        public: Some("DFSFSF".to_string()),
+        private: Some("DFSFSF".to_string()),
+    }
+}
+
+fn create_ipv4() -> String {
+    return "hello".to_string();
+}
