0add38deb1620c76ff01d72d46858ef8da27f193
Alex Greig <78413528+alexjgreig@users.noreply.github.com>
2021-05-08 13:38:36 +1000

    Decided to add the Design Documentation aspect of the project for logging purposes.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let their_public: PublicKey = PublicKey::from([0u8; 32]);
     let shared_secret = SecretKey::from(user.keys.secret_key).diffie_hellman(&their_public);
     let enc_msg = shared_secret.encrypt("Hello".to_string());
+    println!("{:?}", &enc_msg);
     let dec_msg = shared_secret.decrypt(enc_msg);
     println!("{}", dec_msg);
 }
