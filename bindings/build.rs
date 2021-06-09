b51e860286e29aef44fb987616e0d346cc767fd1
Alex Greig <78413528+alexjgreig@users.noreply.github.com>
2021-06-09 20:42:15 +1000

    Utilising the Windows API I interacted with the WiFi interfaces and hardware to list the available networks and their signal strengthin proximity. Within this time period I also fixed a problem with compilation as the windows bindings required microsoft build tools.

--- bindings/build.rs
+++ bindings/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    windows::build!();
+    windows::build!(
+    Windows::Win32::NetworkManagement::WiFi::*,
+    Windows::Win32::System::SystemServices::HANDLE,
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,40 @@ use cryptography::PublicKey;
 use cryptography::SecretKey;
 use node::Node;
 
-fn main() {
-    println!("hello");
+use bindings::Windows::Win32::NetworkManagement::WiFi::WLAN_AVAILABLE_NETWORK_LIST;
+use bindings::Windows::Win32::NetworkManagement::WiFi::WLAN_INTERFACE_INFO_LIST;
+use bindings::Windows::Win32::NetworkManagement::WiFi::*;
+use bindings::Windows::Win32::System::SystemServices::HANDLE;
+use windows::Guid;
+
+use std::ptr;
+use std::thread;
+use std::time::Duration;
+
+fn main() -> windows::Result<()> {
+    let mut handle = HANDLE(1);
+    let mut client: *mut HANDLE = &mut handle;
+    let mut interface: *mut WLAN_INTERFACE_INFO_LIST = ptr::null_mut();
+    let mut avail_nets: *mut WLAN_AVAILABLE_NETWORK_LIST = ptr::null_mut();
+    unsafe {
+        let value = WlanOpenHandle(1u32, ptr::null_mut(), &mut 1u32, client);
+        println!("Return {}", value);
+        let result = WlanEnumInterfaces(*client, ptr::null_mut(), &mut interface);
+        println!("{}", result);
+        let guid: *const Guid = &((*interface).InterfaceInfo[0].InterfaceGuid);
+        let list =
+            WlanGetAvailableNetworkList(*client, guid, 2u32, ptr::null_mut(), &mut avail_nets);
+        println!("{:?}", list);
+
+        // for i in 0..(*avail_nets).dwNumberOfItems {
+        //   println!("{:?}", (*avail_nets).Network[i as usize]);
+        //}
+        println!("{:?}", *(avail_nets));
+
+        thread::sleep(Duration::from_millis(150000000));
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
